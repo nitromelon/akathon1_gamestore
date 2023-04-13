9dc1aed984ecc7bb7fd54985add60a9cb4684ba3
nitromelon <github.z6b5z@simplelogin.com>
2023-04-13 19:16:26 +0700

    Thursday. On branch master Changes to be committed: 	modified:   package.json 	modified:   pnpm-lock.yaml 	modified:   src/lib/main_screen/taskbar/tb.svelte 	modified:   static/favicon.png

--- src/lib/main_screen/taskbar/tb.svelte
+++ src/lib/main_screen/taskbar/tb.svelte
@@ -1,6 +1,16 @@
+<script lang="ts">
+</script>
+
 <div class="taskbar">
-	<!-- todo -->
-	<a href="./about" class="logo">Kirito.</a>
+	<!-- svelte-ignore a11y-mouse-events-have-key-events -->
+	<p class="logo">K.</p>
+	<a href="./introduction" class="link">Introduction</a>
+	<a href="./product" class="link">Product</a>
+	<a href="./payment" class="link">Payment</a>
+	<a href="./about" class="link">About</a>
+	<a href="./more" class="link">
+		<!-- More logo -->
+	</a>
 </div>
 
 <style lang="scss">
@@ -8,37 +18,41 @@
 		position: absolute;
 		bottom: 8px;
 		left: 8px;
-		height: 48px;
+		height: 52px;
 		width: auto;
 		min-width: calc(40 * 8px);
 		background: var(--taskbar_bg);
-		color: white;
 		border-radius: 8px;
 		backdrop-filter: blur(25px);
 		z-index: 1000;
-
 		max-width: calc(100% - 16px);
-
 		padding: 8px;
 		display: flex;
 		align-items: center;
-		column-gap: 16px;
-
+		// column-gap: 0px;
 		// overflow: hidden;
 		// overflow-x: auto;
-		.logo {
+		* {
+			cursor: default;
+		}
+		.logo,
+		.link {
 			position: relative;
 			font-family: var(--font_family);
 			height: 100%;
-			width: fit-content;
-			// background-color: red;
+			width: auto;
 			display: flex;
 			align-items: center;
 			justify-content: center;
-			font-size: 24px;
 			padding: 0 8px;
 			text-decoration: none;
 			color: #1a1a1a;
 		}
-	}
+		.logo {
+			font-size: 24px;
+		}
+		.link {
+			font-size: 18px;
+		}
+	}	
 </style>
